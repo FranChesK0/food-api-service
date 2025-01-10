64e8210530972898ccda12151b6d7f24e7927efd
FranChesK0 <khl_doss@mail.ru>
2025-01-10 11:53:52 +0800

    create tmp implementation of schedule endpoint

--- food/src/routers/places/router.py
+++ food/src/routers/places/router.py
@@ -1,3 +1,20 @@
 from fastapi import APIRouter
 
+from .responses import RestaurantScheduleResponse
+
 router = APIRouter(prefix="/places", tags=["Places"])
+
+
+@router.get("/{restaurant_id}/schedule", summary="")
+async def get_restaurant_schedule(restaurant_id: int) -> RestaurantScheduleResponse:
+    _ = restaurant_id
+    schedule = {
+        "mon": {"start": "10:00", "end": "22:00"},
+        "tue": {"start": "10:00", "end": "22:00"},
+        "wed": {"start": "10:00", "end": "22:00"},
+        "thu": {"start": "10:00", "end": "22:00"},
+        "fri": {"start": "10:00", "end": "22:00"},
+        "sat": {"start": "10:00", "end": "22:00"},
+        "sun": {"start": "10:00", "end": "22:00"},
+    }
+    return RestaurantScheduleResponse.model_validate(schedule)
